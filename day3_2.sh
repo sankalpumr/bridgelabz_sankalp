156f16c3c47543f7c822139c6c74749ed5c27259
unknown <sankalp.umr@gmail.com>
2021-08-02 09:32:40 +0530

    shsh

---
+++ day3_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+for file in `ls *.txt`;
+do 
+	foldername=`echo $file | awk -F. '{print $1}'`;
+	echo $foldername;
+	if [ -d $foldername ]
+then
+	rm -r $foldername;
+fi
+	mkdir $foldername;
+	mv $file $foldername;
+done
+
